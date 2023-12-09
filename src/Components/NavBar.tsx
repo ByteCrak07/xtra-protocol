ba6c8ab610b8de3df0cae0b2cbcc98c4e33c6357
Anjana <anjanakta@gmail.com>
2023-12-09 22:44:02 +0530

    color changed

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,7 +6,8 @@ export default function () {
   return (
     <div className='Nav'>
       <div className='left-nav'>
-        <Image src='/assets/XtraLogo.png' width={30} height={10}  alt='X-tra' />
+        {/* <Image src='/assets/XtraLogo.png' width={30} height={10}  alt='X-tra' /> */}
+        <div id='logo'>X-TRA</div>
         <div className='dashboard-nav'>Dashboard</div>
       </div>
       <div className='right-nav'>
@@ -16,3 +17,4 @@ export default function () {
     
   )
 }
+// 15E265F-A885-4692-BB6B-DE4EEDE2B439
